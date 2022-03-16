065da3f645d9c618690635b685a9614d4d5d0e0c
Sophia9156 <suhyun9156@gmail.com>
2022-03-16 18:42:32 +0900

    loadImage 플러그인 추가

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index.js'
 import store from './store/store.js'
+import loadImage from './plugins/loadImage.js'
 
 createApp(App)
-.use(router)
-.use(store)
+.use(router) // $route, $router
+.use(store) // $store
+.use(loadImage) // $loadImage
 .mount('#app')
--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -20,7 +20,11 @@
     <div v-else class="movie-details">
       <div 
       :style="{backgroundImage: `url(${requestDiffSizeImage(theMovie.Poster)})`}" 
-      class="poster"></div>
+      class="poster">
+        <Loader 
+        v-if="imageLoading" 
+        absolute />
+      </div>
       <div class="specs">
         <div class="title">
           {{theMovie.Title}}
@@ -76,6 +80,11 @@ export default {
   components: {
     Loader
   },
+  data() {
+    return {
+      imageLoading: true
+    }
+  },
   computed: {
     theMovie() {
       return this.$store.state.movie.theMovie
@@ -91,7 +100,12 @@ export default {
   },
   methods: {
     requestDiffSizeImage(url, size = 700) {
-      return url.replace('SX300', `SX${size}`)
+      const src = url.replace('SX300', `SX${size}`)
+      this.$loadImage(src)
+      .then(() => {
+        this.imageLoading = false
+      })
+      return src
     }
   }
 }
@@ -150,6 +164,7 @@ export default {
     background-size: cover;
     background-position: center;
     flex-shrink: 0;
+    position: relative;
   }
   .specs{
     flex-grow: 1;
