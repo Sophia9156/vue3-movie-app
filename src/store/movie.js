6d32f8ae7f4f37028a66c3e254da543b4e72d4c3
Sophia9156 <suhyun9156@gmail.com>
2022-03-16 18:05:33 +0900

    AWS Lambda로 이미지 리사이징

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,7 +8,8 @@ export default {
   state: () => ({
     movies: [],
     message: 'Search for the movie title!',
-    loading: false
+    loading: false,
+    theMovie: {}
   }),
   // computed!
   getters: {},
@@ -66,6 +67,29 @@ export default {
         })
       } finally {
         commit('updateState', {
+          theMovie: {},
+          loading: false
+        })
+      }
+    },
+    async searchMovieWithId({state, commit}, payload) {
+      if(state.loading) return
+
+      commit('updateState', {
+        loading: true
+      })
+
+      try {
+        const res = await _fetchMovie(payload)
+        commit('updateState', {
+          theMovie: res.data
+        })
+      } catch (err) {
+        commit('updateState', {
+          theMovie: {}
+        })
+      } finally {
+        commit('updateState', {
           loading: false
         })
       }
@@ -74,9 +98,11 @@ export default {
 }
 
 function _fetchMovie(payload) {
-  const {title, type, year, page} = payload
+  const {title, type, year, page, id} = payload
   const OMDB_API_KEY = process.env.VUE_APP_OMDB_API_KEY
-  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+  const url = id 
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
 
   return new Promise((resolve, reject) => {
     axios.get(url)
