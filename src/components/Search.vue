6fb570910a31de3821f20f2084c3a5a7676a79d1
Sophia9156 <suhyun9156@gmail.com>
2022-03-17 11:05:00 +0900

    반응형 추가

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -66,6 +66,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../scss/main.scss';
+
 .container{
   display: flex;
   > * {
@@ -90,6 +92,24 @@ export default {
     height: 50px;
     font-weight: 700;
     flex-shrink: 0;
+  }
+
+  @include media-breakpoint-down(lg) {
+    display: block;
+    input{
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
+    .selects{
+      margin-right: 0;
+      margin-bottom: 10px;
+      select{
+        width: 100%;
+      }
+    }
+    .btn{
+      width: 100%;
+    }
   }
 }
 </style>
